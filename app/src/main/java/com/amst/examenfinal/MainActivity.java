61ee55d302316b9b9bb7c9afc7c21bcb8c5f8e50
David Mendoza <drmendoz@espol.edu.ec>
2021-01-29 11:56:47 -0500

    Edit con boton

--- app/src/main/java/com/amst/examenfinal/MainActivity.java
+++ app/src/main/java/com/amst/examenfinal/MainActivity.java
@@ -5,19 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText heroeFiltro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        heroeFiltro= findViewById(R.id.heroeInsertar);
         setContentView(R.layout.activity_main);
     }
 
 
     public void irSugerencias(View view){
+        String heroe= heroeFiltro.getText().toString();
         Intent intent= new Intent(this.getApplicationContext(),Sugerencias.class);
-        intent.putExtra("sugerencia","bat");
+        intent.putExtra("sugerencia",heroe);
         startActivity(intent);
     }
 }
