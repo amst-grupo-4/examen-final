dc904bbf5fd233a54803b8d3e9d3d0a71491bb0d
David Mendoza <drmendoz@espol.edu.ec>
2021-01-29 15:16:00 -0500

    Mensaje de error

--- app/src/main/java/com/amst/examenfinal/MainActivity.java
+++ app/src/main/java/com/amst/examenfinal/MainActivity.java
@@ -2,11 +2,14 @@ package com.amst.examenfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText heroeFiltro;
@@ -20,11 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void irSugerencias(View view){
+
         heroeFiltro= findViewById(R.id.heroeInsertar);
         String heroe= heroeFiltro.getText().toString();
-        Intent intent= new Intent(this.getApplicationContext(),Sugerencias.class);
-        intent.putExtra("sugerencia",heroe);
-        startActivity(intent);
+        if(heroe.length()>0) {
+            Intent intent = new Intent(this.getApplicationContext(), Sugerencias.class);
+            intent.putExtra("sugerencia", heroe);
+            startActivity(intent);
+        }
+        else {
+            CharSequence text = "Por favor ingrese al menos un caracter";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+            toast.show();
+        }
     }
 
 }
